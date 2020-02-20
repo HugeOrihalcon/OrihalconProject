3df89d054cc87d0b0b32b1bd46a60e92890b3793
Daisuke Orita <kikoricontact@yahoo.co.jp>
2020-02-20 10:19:25 +0900

    Initial Commit

---
+++ OrihalconProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrihalconProject
+//
+//  Created by Daisuke Orita on 2020/02/20.
+//  Copyright © 2020 Daisuke Orita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
